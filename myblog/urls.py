e9fbfa0dac2ffc6f906db44e433ca8abcbf6aeb1
Leenok <eseredich8@gmail.com>
2024-11-20 00:45:14 +0300

    feat: add signup

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MainView, AboutView, PostDetailView, ContactView
+from .views import MainView, AboutView, PostDetailView, ContactView, SignUpView
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
     path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('about', AboutView.as_view(), name='about'),
     path('contact', ContactView.as_view(), name='contact'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,10 @@
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-
-from myblog.models import Post
+from django.core.paginator import Paginator
+from .models import Post
+from .forms import SignUpForm
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
@@ -36,3 +38,21 @@ class PostDetailView(View):
         return render(request, 'myblog/post_detail.html', context={
             'post': post
     })
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'myblog/signup.html', context={
+            'form': form
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
